4f2a7c328ebd8cfdfcde25c82ed5635c32fd8178
wsl-thomas <thomasthienbui@gmail.com>
2022-11-08 09:21:47 +0700

    Cleanup code for tests

--- test/test.py
+++ test/test.py
@@ -4,7 +4,6 @@ global correct
 correct = 0
 
 def test(data):
-    files = []
     for src in data[2]:
         with open(src[0], "w") as f:
             f.write(src[1].strip())
@@ -13,16 +12,27 @@ def test(data):
     for src in data[2]:
         cmd += " " + src[0]
     cmd += " > /dev/null"
-    subprocess.run(cmd, shell=True)
-    p = subprocess.run("./out.exe", shell=True)
+
+    subprocess.call(cmd, shell=True)
+    subprocess.call("chmod +x out.exe", shell=True)
+    p = subprocess.call("./out.exe", shell=True)
 
     name = "[" + data[0] + "]"
     result = "Failed"
-    if p.returncode == data[1]:
+    if p == data[1]:
         result = "Passed"
         global correct
         correct += 1
+
     print(name.ljust(40, " "), result)
+
+    #cleanup
+    for src in data[2]:
+        subprocess.call("rm " + src[0], shell=True)
+        subprocess.call("rm " + src[0][:-4] + ".asm", shell=True)
+        subprocess.call("rm " + src[0][:-4] + ".obj", shell=True)
+
+    subprocess.call("rm out.exe", shell=True)
 
 
 tests = [
@@ -34,19 +44,6 @@ tests = [
                             return 0
                         }
                         """
-                    )
-                ]
-            ),
-            ("sign integer arithmetic", 0, 
-                [
-                    ("main.tmd",
-                         """
-                         main: () -> int {
-                            x: int = 10
-                            y: int = 10
-                            return -100 + x * y - x + y - y / x + x / y
-                         }
-                         """
                     )
                 ]
             ),
@@ -129,6 +126,21 @@ tests = [
             ),
         ]
 
+
+
+failed_test = ("sign integer arithmetic", 0, 
+                [
+                    ("main.tmd",
+                         """
+                         main: () -> int {
+                            x: int = 10
+                            y: int = 10
+                            return -100 + x * y - x + y - y / x + x / y
+                         }
+                         """
+                    )
+                ]
+            )
 
 for data in tests:
     test(data)
